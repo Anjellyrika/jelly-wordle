048ed025a434d5e779d29d0eb1fedb6dd0b79d41
Anjellyrika <ajraborar2@ymail.com>
2022-09-28 02:36:25 +0800

    checkpoint1

---
+++ bootleg_wordle.js
@@ -0,0 +1,42 @@
+"use strict";
+function linebreak() {
+    return document.createElement('br');
+}
+const apiEndpoint = "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/ca9018b32e963292473841fb55fd5a62176769b5/valid-wordle-words.txt";
+function startscreen() {
+    const inputdiv = document.getElementById('URL Input');
+    if (inputdiv !== null) {
+        let elements = [];
+        const textInput = document.createElement('input');
+        textInput.setAttribute('type', 'text');
+        textInput.value = apiEndpoint;
+        elements.push(textInput);
+        const startbutton = document.createElement('input');
+        startbutton.setAttribute('type', 'button');
+        startbutton.value = "Get a random word!";
+        elements.push(startbutton);
+        inputdiv.replaceChildren(...elements);
+        // Clicking the start button
+        addEventListener('click', () => {
+            if (textInput.value === '')
+                alert("No URL specified!");
+            else {
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', textInput.value, true);
+                xhr.onload = function () {
+                    // Fetch a random word
+                    const wordlist = (xhr.responseText).split('\n');
+                    const randomIndex = Math.floor(Math.random() * wordlist.length);
+                    console.log(wordlist[randomIndex]);
+                };
+                xhr.send();
+            }
+        });
+    }
+}
+alert("Welcome to bootleg Wordle!"); // debugging
+startscreen();
+// if (textinput.value === '') {
+//     alert("No URL specified.");
+// }
+// elements.push(linebreak());
---
+++ test.js
@@ -0,0 +1,14 @@
+"use strict";
+// TESTING GROUNDS
+// console.log("Test file");
+const testfile_inputdiv = document.getElementById('URL Input');
+if (testfile_inputdiv !== null) {
+    let elements = [];
+    const sortedBox = document.createElement('input');
+    sortedBox.setAttribute('type', 'checkbox');
+    elements.push(sortedBox);
+    const sortedBoxLabel = document.createElement('label');
+    sortedBoxLabel.textContent = "Checkbox";
+    elements.push(sortedBoxLabel);
+    testfile_inputdiv.replaceChildren(...elements);
+}
